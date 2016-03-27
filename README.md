7159e625dd4283344bc48b60e66e4cf6e2f62b4c
Timofey Tipishev <tipishev@yandex-team.ru>
2016-03-27 21:29:51 +0500

    add grid instructions and the second test

--- README.md
+++ README.md
@@ -7,3 +7,22 @@ System Requirements: a Unix-like OS with java and python3.
 ```
 pip3 install -r requrements.txt
 ```
+
+## Launching tests
+```
+# launch the Selenium grid hub
+java -jar selenium-server-standalone-2.52.0.jar -port 4444 -role hub -nodeTimeout 1000
+
+&
+
+# launch the first Firefox node
+java -jar selenium-server-standalone-2.52.0.jar -role node -hub http://localhost:4444/grid/register -browser browserName=firefox -port 5555
+
+&
+
+# launch the second Firefox node
+java -jar selenium-server-standalone-2.52.0.jar -role node -hub http://localhost:4444/grid/register -browser browserName=firefox -port 5556
+
+# run the tests
+py.test -n 2 main.py
+```
--- main.py
+++ main.py
@@ -11,6 +11,8 @@ from pages import (
         AddToShoppingCartPage
 )
 
+from config import log
+
 REMOTE_DRIVER_ENDPOINT = "http://127.0.0.1:4444/wd/hub"
 MAIN_PAGE_URL = "https://www.sportamore.se"
 
@@ -24,7 +26,7 @@ class SportamoreTestCase(unittest.TestCase):
         )
         self.driver.get(MAIN_PAGE_URL)
 
-    def test_upsell_on_featured_product(self):
+    def test_upsell_on_featured_womens_product(self):
         CONSUMER = "Dam"
         PRODUCT_CATEGORY = "Underkläder"
         EXPECTED_URL = "https://www.sportamore.se/dam/klader/underklader/"
@@ -58,9 +60,42 @@ class SportamoreTestCase(unittest.TestCase):
                 )
         )
 
-        def tearDown(self):
-            self.driver.quit()
+    def test_upsell_on_featured_mens_product(self):
+        CONSUMER = "Herr"
+        PRODUCT_CATEGORY = "Byxor"
+        EXPECTED_URL = "https://www.sportamore.se/herr/klader/byxor/"
+        EXPECTED_UPSELL = {'Slim 500ml', '3Ppk Value No Show'}
 
+        driver = self.driver
+
+        # navigate from the Main page to "herr/byxor" via hover-menu
+        main_page = MainPage(driver)
+        main_page.go_to_consumer_product_category(CONSUMER, PRODUCT_CATEGORY)
+
+        # click on the featured product
+        consumer_product_category_page = ConsumerProductCategoryPage(driver)
+        current_url = consumer_product_category_page.current_url
+        # self.assertEqual(current_url, EXPECTED_URL)
+        consumer_product_category_page.go_to_featured_product()
+
+        # proceed to quantity selection
+        product_page = ProductPage(driver)
+        # ↓ an abstraction leak to demo flexibility
+        product_page.size_select.select_first_size()
+        product_page.go_to_add_to_cart()
+
+        # check the actual upsell products against expected
+        add_to_cart_page = AddToShoppingCartPage(driver)
+        actual_upsell = add_to_cart_page.get_upsell_items()
+
+        self.assertTrue(EXPECTED_UPSELL.issubset(actual_upsell),
+                "Unexpected upsell items: {}".format(
+                    EXPECTED_UPSELL.difference(actual_upsell)
+                )
+        )
+
+    def tearDown(self):
+        self.driver.quit()
 
 if __name__ == "__main__":
     unittest.main()
